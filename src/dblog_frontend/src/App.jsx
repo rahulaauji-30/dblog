03733b6cd710f3c3fb8cba65bcae95af040692fd
Rahul Parihar <rahulaauji71@gmail.com>
2024-03-28 21:41:58 +0530

    Update App.jsx

--- src/dblog_frontend/src/App.jsx
+++ src/dblog_frontend/src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import { dblog_backend } from "declarations/dblog_backend";
 import Navbar from "./components/Navbar";
-import { Outlet } from "react-router-dom";
 import Post from "./components/Post";
 import Blogs from "./components/Blogs";
 import Admin from "./components/Admin";
 import NewPost from "./components/NewPost";
+import { Outlet } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <main>
       <Navbar />
-      <NewPost/>
+      <HashRouter future={{ v7_startTransition: true }}>
+      <Routes>
+          <Route path="/" element={<Admin />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/newpost" element={<NewPost />} />
+          <Route path="/post" element={<Post />} />
+        </Routes>
+      </HashRouter>
     </main>
   );
 }
