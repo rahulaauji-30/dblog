60f83cf0dd43c657e56a1050f0144e0f48fde9c5
Rahul Parihar <rahulaauji71@gmail.com>
2024-03-28 21:40:12 +0530

    Update Blog.jsx

--- src/dblog_frontend/src/components/Blog.jsx
+++ src/dblog_frontend/src/components/Blog.jsx
@@ -1,14 +1,16 @@
 
 import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
 import XIcon from "@mui/icons-material/X";
+import { NavLink } from "react-router-dom";
 export default function Blog(props) {
   return (
     <div className="blog">
       <img src={props.img} />
-      <div className="blog-titles">
-        <span>{props.title}</span>
+      <div  className="blog-titles">
+        <NavLink to="/post">{props.title}</NavLink>
         <p>{props.date}| {props.author}</p>
       </div>
+      
     </div>
   );
 }
