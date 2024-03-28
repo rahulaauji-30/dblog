a2d945319bea35523c144d91be332c7d2cf279d2
Rahul Parihar <rahulaauji71@gmail.com>
2024-03-28 21:39:51 +0530

    Update Admin.jsx

--- src/dblog_frontend/src/components/Admin.jsx
+++ src/dblog_frontend/src/components/Admin.jsx
@@ -1,12 +1,15 @@
+import { NavLink } from "react-router-dom";
 import Blogs from "./Blogs";
-
+import { dblog_backend } from "../../../declarations/dblog_backend";
+import { useEffect } from "react";
+import BlogsAdmin from "./BlogsAdmin";
 export default function Admin(){
-
+    
     return <>
        <div className="admin-post">
-       <a className="new-post">New Post</a>
+       <NavLink to="/newpost" className="new-post">New Post</NavLink>
         <div className="blog-main">
-        <Blogs/>
+        <BlogsAdmin/>
         </div>
        </div>
     </>
