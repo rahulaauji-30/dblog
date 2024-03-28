4d9fd7d2561bed75f3274a112b6f6dd83ebeabb8
Rahul Parihar <rahulaauji71@gmail.com>
2024-03-28 21:40:33 +0530

    Update Blogs.jsx

--- src/dblog_frontend/src/components/Blogs.jsx
+++ src/dblog_frontend/src/components/Blogs.jsx
@@ -1,13 +1,34 @@
 import Blog from "./Blog";
+import { dblog_backend } from "../../../declarations/dblog_backend";
 import D from "../../public/3D.jpg";
-import adventure from "../../public/adventure.jpg"
-import space from "../../public/space.jpg"
+import adventure from "../../public/adventure.jpg";
+import space from "../../public/space.jpg";
 import Blockchain from "../../public/blockchain.jpeg";
-export default function Blogs(){
-    return<>
-        <Blog img={D} title="The Inner World of the Atom" author="Chat GPT" date="1 March 2024"/>
-        <Blog img={adventure} title="The 10 best adventure places" author="Gemini" date="23 March 2024"/>
-        <Blog img={space} title="Journeys Through the Cosmos: A Glimpse of a Planetary System" author="Rahul Aauji" date="4 April 2024"/>
-        <Blog img={Blockchain} title="Blockchain Explained: A Simple Guide" author="Chat GPT" date="1 March 2024"/>
+import { useEffect, useState } from "react";
+export default function Blogs() {
+  const [blogs, setBlogs] = useState([]);
+  const images = [D,space,adventure,Blockchain]
+  useEffect(() => {
+    fetchData();
+    console.log("Admin Triggered");
+  }, []);
+  async function fetchData() {
+    const data = await dblog_backend.getData();
+    setBlogs(data);
+  }
+  return (
+    <>
+      {blogs.map((blog,index) => {
+        
+        return (
+          <Blog
+            img={images[Math.floor(Math.random() * images.length)]}
+            title={blog.Title}
+            author={blog.Author}
+            date={blog.Date}
+          />
+        );
+      })}
     </>
+  );
 }
