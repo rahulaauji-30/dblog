d8d5b65e5d92aa44348f47340fe2a341d6025807
Rahul Parihar <rahulaauji71@gmail.com>
2024-03-28 21:41:02 +0530

    Update Navbar.jsx

--- src/dblog_frontend/src/components/Navbar.jsx
+++ src/dblog_frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import pic from "../../public/profile.png"
 import DragHandleIcon from '@mui/icons-material/DragHandle';
+import { NavLink } from "react-router-dom";
 export default function Navbar(){
     const [isTrue,setTrue] = useState(false)
     function handleClick(){
@@ -8,7 +9,7 @@ export default function Navbar(){
     }
     return<>
         <nav className="navbar">
-            <img src={pic}/>
+        
             <p>Blogs</p>
             {
                 isTrue?<button>Log In </button>:<DragHandleIcon style={{color:"green",cursor:"pointer"}} onClick={handleClick} fontSize="large"/>
